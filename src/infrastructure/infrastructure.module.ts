04bcf85b8861de180ad522bf04c491f8c83c4a15
migueltorresd <migueltorresdiaz1992@gmail.com>
2023-04-09 02:51:47 -0500

    Titulo: Se crea exception-filter  -descipcion: Se cra exception-filter y se realizan pruebas

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -3,10 +3,15 @@ import { PersistenceModule } from './persistence/persistence.module';
 import { HealthcareProviderController } from './controllers/healthcare-provider.controllers';
 import { PatientController } from './controllers/patient.controllers';
 import { AppointmentController } from './controllers/appointment.controllers';
+import { APP_FILTER } from '@nestjs/core';
+import { MongoServerErrorExceptionFilter } from './utils/exception-filters/mongo-server-error.exception-filter';
 
 @Module({
   imports: [PersistenceModule],
   controllers: [HealthcareProviderController, PatientController, AppointmentController],
-  providers: [],
+  providers: [{
+    provide: APP_FILTER,
+    useClass: MongoServerErrorExceptionFilter,
+  },],
 })
 export class InfrastructureModule {}
